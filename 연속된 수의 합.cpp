ffe64ba50953b50b71a29106f1c3b62cf79c3a1c
shin0112 <83761128+shin0112@users.noreply.github.com>
2024-01-16 02:44:14 +0900

    Update 연속된 수의 합.cpp
    출처 수정

--- 연속된 수의 합.cpp
+++ 연속된 수의 합.cpp
@@ -22,5 +22,5 @@ vector<int> solution(int num, int total) {
     return answer;
 }
 
-// 출처 : 프로그래머스 코딩 테스트 - 연속된 수의 합
+// 출처 : 프로그래머스 코딩 테스트 연습 - 연속된 수의 합
 // https://school.programmers.co.kr/learn/courses/30/lessons/120923
