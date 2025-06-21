14f00a54c0dc8c68453ad61781107cab3e2f2940
shin0112 <wnslcosltimo12@gmail.com>
2025-06-21 17:32:48 +0900

    style: google style 포맷하기

--- java/Lv0/181949/Solution.java
+++ java/Lv0/181949/Solution.java
@@ -1,18 +1,20 @@
+
 import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String answer = "";
-        
+
         for (int i = 0; i < a.length(); i++) {
             char c = a.charAt(i);
             answer += Character.isUpperCase(c)
-                ? Character.toLowerCase(c) 
-                : Character.toUpperCase(c);        
+                    ? Character.toLowerCase(c)
+                    : Character.toUpperCase(c);
         }
-        
+
         System.out.println(answer);
     }
 }
